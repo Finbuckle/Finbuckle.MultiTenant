aa584f71705fba750a2fae1e8eab8aa00c6c5922
Andrew White <achandlerwhite@gmail.com>
2018-05-29 19:51:09 -0400

    Renamed sample projects

---
+++ samples/RouteStrategySample/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RouteStrategySample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMultiTenant().
+                WithInMemoryStore(Configuration.GetSection("Finbuckle:MultiTenant:InMemoryMultiTenantStore")).
+                WithRouteStrategy();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMultiTenant(ConfigRoutes);
+
+            app.UseMvc(ConfigRoutes);
+        }
+
+        private void ConfigRoutes(IRouteBuilder routes)
+        {
+            routes.MapRoute("Defaut", "{__tenant__=}/{controller=Home}/{action=Index}");
+        }
+    }
+}
