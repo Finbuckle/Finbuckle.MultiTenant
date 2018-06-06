f7022f8a21b969a737a1ec6c8a22cd963cf16a1f
Andrew White <achandlerwhite@gmail.com>
2018-06-05 23:27:26 -0400

    AuthOptions sample work

--- samples/AuthenticationOptionsSample/Controllers/HomeController.cs
+++ samples/AuthenticationOptionsSample/Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using Finbuckle.MultiTenant.AspNetCore;
 using Finbuckle.MultiTenant.Core;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace AuthenticationOptionsSample.Controllers
@@ -14,10 +16,10 @@ namespace AuthenticationOptionsSample.Controllers
     {
         public IActionResult Index()
         {
-            var tc = HttpContext.GetTenantContextAsync().Result;   
+            var tc = HttpContext.GetTenantContext();
             var title = (tc?.Name ?? "No tenant") + " - ";
 
-            if(!User.Identity.IsAuthenticated)
+            if (!User.Identity.IsAuthenticated)
             {
                 title += "Not Authenticated";
             }
@@ -28,17 +30,29 @@ namespace AuthenticationOptionsSample.Controllers
 
             ViewData["Title"] = title;
 
-            var cookieOptionsMonitor = (IOptionsMonitor<CookieAuthenticationOptions>)HttpContext.RequestServices.GetService(typeof(IOptionsMonitor<CookieAuthenticationOptions>));
-            var cookieName = cookieOptionsMonitor.Get(CookieAuthenticationDefaults.AuthenticationScheme).Cookie.Name;
-            ViewData["CookieName"] = cookieName;
-            
+            if (tc != null)
+            {
+                var cookieOptionsMonitor = HttpContext.RequestServices.GetService<IOptionsMonitor<CookieAuthenticationOptions>>();
+                var cookieName = cookieOptionsMonitor.Get("cookies").Cookie.Name;
+                ViewData["CookieName"] = cookieName;
+
+                var schemes = HttpContext.RequestServices.GetRequiredService<IAuthenticationSchemeProvider>();
+                ViewData["ChallengeScheme"] = schemes.GetDefaultChallengeSchemeAsync().Result.Name;
+            }
+
             return View(tc);
+        }
+
+        [Authorize]
+        public IActionResult Authenticate()
+        {
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> Login()
         {
             await HttpContext.SignOutAsync();
-            await HttpContext.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity(new[]{ new Claim(ClaimTypes.Name, "Username") }, "Cookie")));
+            await HttpContext.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "Username") }, "Cookie")));
             return RedirectToAction("Index");
         }
 
