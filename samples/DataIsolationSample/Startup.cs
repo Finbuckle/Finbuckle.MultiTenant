944c85c4413d53163cf878492a17551ef5714276
Andrew White <achandlerwhite@gmail.com>
2018-12-11 18:43:30 -0700

    chore: update projects for .net core 2.2
    Various changes for .Net Core 2.2:
    - update sampe projects
    - change core libs to depend on 2.* assembly versions
    
    Closes #84

--- samples/DataIsolationSample/Startup.cs
+++ samples/DataIsolationSample/Startup.cs
@@ -23,7 +23,7 @@ namespace DataIsolationSample
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddMultiTenant().
                 WithInMemoryStore(Configuration.GetSection("Finbuckle:MultiTenant:InMemoryStore")).
