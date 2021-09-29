aceff1d73540b22ef64c6cec0fd50e43eff5387b
Andrew White <andrew@finbuckle.com>
2021-09-29 17:49:38 -0600

    fix: revert some platform targets to netstandard (#469)
    changed aspnetcore3.1 targets to netstandard2.1 (except aspnetcore project)

--- src/Finbuckle.MultiTenant.EntityFrameworkCore/Extensions/EntityTypeBuilderExtensions.cs
+++ src/Finbuckle.MultiTenant.EntityFrameworkCore/Extensions/EntityTypeBuilderExtensions.cs
@@ -146,7 +146,7 @@ namespace Finbuckle.MultiTenant.EntityFrameworkCore
             builder.RemoveIndex("NormalizedUserName");
  #if NET // Covers .NET 5.0 and later.
             builder.HasIndex("NormalizedUserName", "TenantId").HasDatabaseName("UserNameIndex").IsUnique();
- #elif NETCOREAPP3_1 // .NET Core 3.1
+ #elif NETSTANDARD2_1 // .NET Core 3.1
             builder.HasIndex("NormalizedUserName", "TenantId").HasName("UserNameIndex").IsUnique();
  #endif
         }
@@ -156,7 +156,7 @@ namespace Finbuckle.MultiTenant.EntityFrameworkCore
             builder.RemoveIndex("NormalizedName");
 #if NET // Covers .NET 5.0 and later.
             builder.HasIndex("NormalizedName", "TenantId").HasDatabaseName("RoleNameIndex").IsUnique();
-#elif NETCOREAPP3_1 // .NET Core 3.1
+#elif NETSTANDARD2_1 // .NET Core 3.1
             builder.HasIndex("NormalizedName", "TenantId").HasName("RoleNameIndex").IsUnique();
 #endif
         }
