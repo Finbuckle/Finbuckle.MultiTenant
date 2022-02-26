399c0bdc2b5a46dc11eb2545908be800cc86de8e
Andrew White <andrew@finbuckle.com>
2022-02-25 18:36:35 -0700

    chore: added net6.0 BasePathStrategySample and updated Docs

---
+++ samples/net6.0/BasePathStrategySample/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Finbuckle.MultiTenant;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BasePathStrategySample.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    
+    public TenantInfo? TenantInfo { get; set; }
+    public IEnumerable<TenantInfo> Tenants { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    
+    public void OnGet()
+    {
+        TenantInfo = HttpContext.GetMultiTenantContext<TenantInfo>()?.TenantInfo;
+        var store = HttpContext.RequestServices.GetRequiredService<IMultiTenantStore<TenantInfo>>();
+        Tenants = store.GetAllAsync().Result;
+    }
+}
