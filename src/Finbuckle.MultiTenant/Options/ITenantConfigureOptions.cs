06bd19f2e9911369e71a73ca9c657fd261180796
Andrew White <andrew@finbuckle.com>
2022-03-05 22:29:40 -0700

    chore: mark unnamed tenant options types obsolete

--- src/Finbuckle.MultiTenant/Options/ITenantConfigureOptions.cs
+++ src/Finbuckle.MultiTenant/Options/ITenantConfigureOptions.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright Finbuckle LLC, Andrew White, and Contributors.
 // Refer to the solution LICENSE file for more inforation.
 
+using System;
+
 namespace Finbuckle.MultiTenant.Options
 {
+    [Obsolete]
     public interface ITenantConfigureOptions<TOptions, TTenantInfo>
         where TOptions : class, new()
         where TTenantInfo : class, ITenantInfo, new()
