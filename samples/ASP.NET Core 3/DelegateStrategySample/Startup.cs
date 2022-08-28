ec8e08e2bc99a85d28fc8be4aa34070f8eae4437
Andrew White <andrew@finbuckle.com>
2022-08-27 21:03:59 -0600

    fix: add missing using statement to samples (#581)

--- samples/ASP.NET Core 3/DelegateStrategySample/Startup.cs
+++ samples/ASP.NET Core 3/DelegateStrategySample/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Primitives;
 
 namespace DelegateStrategySample
