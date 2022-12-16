e286301a0b4f0180cab5d9e702cbba110d3e4806
Stéphane Mitermite <stephane.mitermite@berger-levrault.com>
2022-12-16 19:11:43 +0100

    Adds StrategyInfo property to IMultiTenantContext interface

--- src/Finbuckle.MultiTenant/Abstractions/IMultiTenantContext.cs
+++ src/Finbuckle.MultiTenant/Abstractions/IMultiTenantContext.cs
@@ -6,13 +6,14 @@ namespace Finbuckle.MultiTenant
     public interface IMultiTenantContext
     {
         ITenantInfo? TenantInfo { get; }
+        StrategyInfo? StrategyInfo { get; }
     }
 
     public interface IMultiTenantContext<T> : IMultiTenantContext
         where T : class, ITenantInfo, new()
     {
         new T? TenantInfo { get; set; }
-        StrategyInfo? StrategyInfo { get; set; }
+        new StrategyInfo? StrategyInfo { get; set; }
         StoreInfo<T>? StoreInfo { get; set; }
     }
 }
