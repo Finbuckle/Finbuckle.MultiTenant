4d15e1fcfdbdb979114128717b42ca3aba819301
Sam Goldmann <sam.goldmann@gmail.com>
2023-07-17 00:42:58 -0400

    Restored commented test

--- test/Finbuckle.MultiTenant.Test/DependencyInjection/ServiceCollectionShould.cs
+++ test/Finbuckle.MultiTenant.Test/DependencyInjection/ServiceCollectionShould.cs
@@ -1,40 +1,37 @@
-﻿//Copyright Finbuckle LLC, Andrew White, and Co ntributors.
+﻿// Copyright Finbuckle LLC, Andrew White, and Co ntributors.
 // Refer to the solution LICENSE file for more information.
 
-//using Finbuckle.MultiTenant.Strategies;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Options;
-//using System.IO.Compression;
-//using Xunit;
+using Finbuckle.MultiTenant.Strategies;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System.IO.Compression;
+using Xunit;
 
-//namespace Finbuckle.MultiTenant.Test.DependencyInjection
-//{
-//    public class ServiceCollectionShould
-//    {
-//        Used in some tests.
-//        ReSharper disable once UnusedAutoPropertyAccessor.Local
-//        private string? TestProperty { get; set; }
+namespace Finbuckle.MultiTenant.Test.DependencyInjection
+{
+    public class ServiceCollectionShould
+    {
+        // Used in some tests.
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        private string? TestProperty { get; set; }
+        
+        [Fact]
+        public void AddPerTenantOptions()
+        {
+            var services = new ServiceCollection();
+            _ = services.AddMultiTenant<TenantInfo>();
 
-//        [Fact]
-//        public void AddPerTenantOptions()
-//        {
-//            var services = new ServiceCollection();
-//            _ = services.AddMultiTenant<TenantInfo>();
+            services.AddPerTenantOptions<ServiceCollectionShould>()
+                .Configure<TenantInfo>((o, ti) => o.TestProperty = ti.Id);
+            
+            var sp = services.BuildServiceProvider();
+            var multiTenantContextAccessor = sp.GetRequiredService<IMultiTenantContextAccessor<TenantInfo>>();
+            multiTenantContextAccessor.MultiTenantContext = new MultiTenantContext<TenantInfo>{ TenantInfo = new TenantInfo { Id = "initech" } };
 
-//            services.AddPerTenantOptions<ServiceCollectionShould>()
-//                .Configure<TenantInfo>((o, ti) => o.TestProperty = ti.Id);
+            using var scope = sp.CreateScope();
+            var options = scope.ServiceProvider.GetRequiredService<IOptions<ServiceCollectionShould>>();
 
-//            var sp = services.BuildServiceProvider();
-//            var multiTenantContextAccessor = sp.GetRequiredService<IMultiTenantContextAccessor<TenantInfo>>();
-//            multiTenantContextAccessor.MultiTenantContext = new MultiTenantContext<TenantInfo>
-//            {
-//                TenantInfo = new TenantInfo { Id = "initech" }
-//            };
-
-//            using var scope = sp.CreateScope();
-//            var options = scope.ServiceProvider.GetRequiredService<IOptions<ServiceCollectionShould>>();
-
-//            Assert.Equal("initech", options.Value.TestProperty);
-//        }
-//    }
-//}
+            Assert.Equal("initech", options.Value.TestProperty);
+        }
+    }
+}
