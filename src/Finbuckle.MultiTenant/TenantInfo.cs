5dd4071880a365e15fb331e0c04a65a4683dc02b
Andrew White <andrew@finbuckle.com>
2024-04-28 09:36:34 -0600

    revert "feat: improve XML comments for all public facing types and methods (#…" (#817)
    This reverts commit 61b374bf70bb1dfaba99caf3866cfcfa1ee7d609.

--- src/Finbuckle.MultiTenant/TenantInfo.cs
+++ src/Finbuckle.MultiTenant/TenantInfo.cs
@@ -6,24 +6,14 @@ using Finbuckle.MultiTenant.Internal;
 
 namespace Finbuckle.MultiTenant;
 
-/// <summary>
-/// The TenantInfo class implements the ITenantInfo interface and represents a basic tenant in a multi-tenant application.
-/// </summary>
 public class TenantInfo : ITenantInfo
 {
     private string? id;
 
-    /// <summary>
-    /// Initializes a new instance of the TenantInfo class.
-    /// </summary>
     public TenantInfo()
     {
     }
 
-    /// <summary>
-    /// Gets or sets the ID of the tenant. The setter validates the length of the ID.
-    /// </summary>
-    /// <exception cref="MultiTenantException">Thrown when the ID exceeds the maximum length.</exception>
     public string? Id
     {
         get
@@ -43,13 +33,6 @@ public class TenantInfo : ITenantInfo
         }
     }
 
-    /// <summary>
-    /// Gets or sets the identifier of the tenant.
-    /// </summary>
     public string? Identifier { get; set; }
-
-    /// <summary>
-    /// Gets or sets the name of the tenant.
-    /// </summary>
     public string? Name { get; set; }
 }
