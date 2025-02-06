709b0deda80ce66d8096b53d42b10b83b0bf5297
Aaron <hansenag02@gmail.com>
2025-02-05 21:45:11 -0600

    feat: Add ShortCircuitWhen(Action<ShortCircuitWhenOptions> options) and ShortCircuitWhenTenantNotResolved()

--- src/Finbuckle.MultiTenant.AspNetCore/Extensions/MultiTenantBuilderExtensions.cs
+++ src/Finbuckle.MultiTenant.AspNetCore/Extensions/MultiTenantBuilderExtensions.cs
@@ -24,6 +24,39 @@ namespace Finbuckle.MultiTenant;
 public static class MultiTenantBuilderExtensions
 {
     /// <summary>
+    /// Configures a callback that determines when endpoints should be short circuited
+    /// during MultiTenant resolution.
+    /// </summary>
+    /// <typeparam name="TTenantInfo">The ITenantInfo implementation type.</typeparam>
+    /// <param name="builder">The <see cref="MultiTenantBuilder&lt;TTenantInfo&gt;"/> instance.</param>
+    /// <param name="options">The short circuit options to configure.</param>
+    /// <returns>The <see cref="MultiTenantBuilder&lt;TTenantInfo&gt;"/> so that additional calls can be chained.</returns>
+    public static MultiTenantBuilder<TTenantInfo> ShortCircuitWhen<TTenantInfo>(
+        this MultiTenantBuilder<TTenantInfo> builder, Action<ShortCircuitWhenOptions> options)
+        where TTenantInfo : class, ITenantInfo, new()
+    {
+        builder.Services.Configure(options);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Configures endpoints to be short circuited during MultiTenant resolution when
+    /// no Tenant was resolved.
+    /// </summary>
+    /// <typeparam name="TTenantInfo">The ITenantInfo implementation type.</typeparam>
+    /// <param name="builder">The <see cref="MultiTenantBuilder&lt;TTenantInfo&gt;"/> instance.</param>
+    /// <returns>The <see cref="MultiTenantBuilder&lt;TTenantInfo&gt;"/> so that additional calls can be chained.</returns>
+    public static MultiTenantBuilder<TTenantInfo> ShortCircuitWhenTenantNotResolved<TTenantInfo>(
+        this MultiTenantBuilder<TTenantInfo> builder)
+        where TTenantInfo : class, ITenantInfo, new()
+    {
+        builder.Services.Configure<ShortCircuitWhenOptions>(config => config.Predicate = context => !context.IsResolved);
+
+        return builder;
+    }
+
+    /// <summary>
     /// Configures authentication options to enable per-tenant behavior.
     /// </summary>
     /// <typeparam name="TTenantInfo">The ITenantInfo implementation type.</typeparam>
