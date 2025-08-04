0d1b68c135052a206e52e6f2bd68f8b813f5d6b7
Andrew White <andrew@finbuckle.com>
2025-08-04 12:54:08 -0600

    fix: support decorated multiple registered services of same type and fix activator bugs for instance and factory based services (#994)

--- test/Finbuckle.MultiTenant.Test/DependencyInjection/ServiceCollectionExtensionsShould.cs
+++ test/Finbuckle.MultiTenant.Test/DependencyInjection/ServiceCollectionExtensionsShould.cs
@@ -125,4 +125,78 @@ public class ServiceCollectionExtensionsShould
             Assert.Single(config);
             Assert.Null(config.Select(c => (ConfigureNamedOptions<TestOptions, IMultiTenantContextAccessor<TenantInfo>>)c).Single().Name);
         }
+        
+    [Fact]
+    public void DecorateService_WithImplementationType_WrapsService()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<ITestService, TestService>();
+        bool result = services.DecorateService<ITestService, DecoratedTestService>();
+        Assert.True(result);
+        var provider = services.BuildServiceProvider();
+        var service = provider.GetRequiredService<ITestService>();
+        Assert.IsType<DecoratedTestService>(service);
+        Assert.IsType<TestService>(((DecoratedTestService)service).Inner);
+    }
+
+    [Fact]
+    public void DecorateService_WithImplementationInstance_WrapsService()
+    {
+        var instance = new TestService();
+        var services = new ServiceCollection();
+        services.AddSingleton<ITestService>(instance);
+        bool result = services.DecorateService<ITestService, DecoratedTestService>();
+        Assert.True(result);
+        var provider = services.BuildServiceProvider();
+        var service = provider.GetRequiredService<ITestService>();
+        Assert.IsType<DecoratedTestService>(service);
+        Assert.Same(instance, ((DecoratedTestService)service).Inner);
+    }
+
+    [Fact]
+    public void DecorateService_WithImplementationFactory_WrapsService()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton<ITestService>(sp => new TestService());
+        bool result = services.DecorateService<ITestService, DecoratedTestService>();
+        Assert.True(result);
+        var provider = services.BuildServiceProvider();
+        var service = provider.GetRequiredService<ITestService>();
+        Assert.IsType<DecoratedTestService>(service);
+        Assert.IsType<TestService>(((DecoratedTestService)service).Inner);
+    }
+
+    [Fact]
+    public void DecorateService_ThrowsIfNoServiceFound()
+    {
+        var services = new ServiceCollection();
+        Assert.Throws<ArgumentException>(() => services.DecorateService<ITestService, DecoratedTestService>());
+    }
+
+    [Fact]
+    public void DecorateService_WrapsAllServicesOfSameType()
+    {
+        var services = new ServiceCollection();
+        var instance1 = new TestService();
+        var instance2 = new TestService();
+        services.AddSingleton<ITestService>(instance1);
+        services.AddSingleton<ITestService>(instance2);
+        bool result = services.DecorateService<ITestService, DecoratedTestService>();
+        Assert.True(result);
+        var provider = services.BuildServiceProvider();
+        var all = provider.GetServices<ITestService>().ToList();
+        Assert.Equal(2, all.Count);
+        Assert.All(all, s => Assert.IsType<DecoratedTestService>(s));
+        var inners = all.Cast<DecoratedTestService>().Select(d => d.Inner).ToList();
+        Assert.Contains(instance1, inners);
+        Assert.Contains(instance2, inners);
+    }
+
+    public interface ITestService { }
+    public class TestService : ITestService { }
+    public class DecoratedTestService : ITestService
+    {
+        public ITestService Inner { get; }
+        public DecoratedTestService(ITestService inner) => Inner = inner;
+    }
 }
