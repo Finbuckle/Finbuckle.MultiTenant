041ba265a07eea6dae0d597e369a1fb90f046185
Andrew White <andrew@finbuckle.com>
2026-07-02 17:17:02 -0600

    test(efcore): cover new DbContext registration and constructor behavior

--- test/Finbuckle.MultiTenant.EntityFrameworkCore.Test/Extensions/MultiTenantDbContextExtensions/MultiTenantDbContextExtensionShould.cs
+++ test/Finbuckle.MultiTenant.EntityFrameworkCore.Test/Extensions/MultiTenantDbContextExtensions/MultiTenantDbContextExtensionShould.cs
@@ -31,7 +31,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantNotSetMode.Throw, should act as Overwrite when adding
             using (var db = new TestDbContext(tenant1, _options))
@@ -71,7 +71,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantNotSetMode.Throw, should act as Overwrite when adding
             using (var db = new TestDbContext(tenant1, _options))
@@ -111,7 +111,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantMismatchMode.Throw
             using (var db = new TestDbContext(tenant1, _options))
@@ -167,7 +167,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantNotSetMode.Throw
             using (var db = new TestDbContext(tenant1, _options))
@@ -214,7 +214,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantMismatchMode.Throw
             using (var db = new TestDbContext(tenant1, _options))
@@ -278,7 +278,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantNotSetMode.Throw
             using (var db = new TestDbContext(tenant1, _options))
@@ -326,7 +326,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             // TenantMismatchMode.Throw
             using (var db = new TestDbContext(tenant1, _options))
@@ -348,10 +348,16 @@ public class MultiTenantDbContextExtensionsShould
             // TenantMismatchMode.Ignore
             using (var db = new TestDbContext(tenant1, _options))
             {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                db.SaveChanges();
+
                 db.TenantMismatchMode = TenantMismatchMode.Ignore;
-                var blog1 = db.Blogs?.First();
-                db.Entry(blog1!).Property("TenantId").CurrentValue = "17";
-                db.Blogs?.Remove(blog1!);
+                db.Entry(blog1).Property("TenantId").CurrentValue = "17";
+                db.Blogs?.Remove(blog1);
 
                 Assert.Equal(1, db.SaveChanges());
             }
@@ -382,7 +388,7 @@ public class MultiTenantDbContextExtensionsShould
     [Fact]
     public void AddOneTrackingHandlerWhenEnforceMultiTenantOnTrackingCalledTwice()
     {
-        var tenant = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        var tenant = new TenantInfo { Id = "abc", Identifier = "abc" };
         using var db = new TestDbContext(tenant, _options);
 
         db.EnforceMultiTenantOnTracking();
@@ -397,14 +403,14 @@ public class MultiTenantDbContextExtensionsShould
 
         Assert.NotNull(stateManager);
 
-        var trackingDelegateField = stateManager!.GetType()
+        var trackingDelegateField = stateManager.GetType()
             .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
             .FirstOrDefault(f => typeof(MulticastDelegate).IsAssignableFrom(f.FieldType) &&
                                  f.Name.Contains("Tracking", StringComparison.OrdinalIgnoreCase));
 
         Assert.NotNull(trackingDelegateField);
 
-        var trackingDelegate = trackingDelegateField!.GetValue(stateManager) as MulticastDelegate;
+        var trackingDelegate = trackingDelegateField.GetValue(stateManager) as MulticastDelegate;
         Assert.Equal(1, trackingDelegate?.GetInvocationList().Length ?? 0);
     }
 
@@ -414,7 +420,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             using var db = new TestDbContext(tenant, _options);
             db.Database.EnsureDeleted();
@@ -439,7 +445,7 @@ public class MultiTenantDbContextExtensionsShould
         try
         {
             _connection.Open();
-            var tenant = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+            var tenant = new TenantInfo { Id = "abc", Identifier = "abc" };
 
             using var db = new TestDbContext(tenant, _options);
             db.Database.EnsureDeleted();
@@ -458,4 +464,355 @@ public class MultiTenantDbContextExtensionsShould
         }
     }
 
+    [Fact]
+    public void ThrowWhenTenantInfoNullAndEntitiesChangedOnSave()
+    {
+        try
+        {
+            _connection.Open();
+            var tenant = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            using var db = new TestDbContext(tenant, _options);
+            db.Database.EnsureCreated();
+            db.TenantInfo = null;
+
+            db.Blogs?.Add(new Blog { Title = "test" });
+            Assert.Throws<MultiTenantException>(() => db.SaveChanges());
+        }
+        finally
+        {
+            _connection.Close();
+        }
+    }
+
+    [Fact]
+    public void ThrowWhenTenantInfoNullOnTracking()
+    {
+        try
+        {
+            _connection.Open();
+            var tenant = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            using var db = new TestDbContext(tenant, _options);
+            db.Database.EnsureCreated();
+            db.EnforceMultiTenantOnTracking();
+            db.TenantInfo = null;
+
+            Assert.Throws<MultiTenantException>(() => db.Add(new Blog { Title = "test" }));
+        }
+        finally
+        {
+            _connection.Close();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantNotSetWhenAddingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantNotSetMode.Throw, should act as Overwrite when adding
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                db.TenantNotSetMode = TenantNotSetMode.Throw;
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+                Assert.Equal(tenant1.Id, db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+
+            // TenantNotSetMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                db.TenantNotSetMode = TenantNotSetMode.Overwrite;
+
+                var blog1 = new Blog { Title = "abc2" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+                Assert.Equal(tenant1.Id, db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantMismatchWhenAddingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantMismatchMode.Throw
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                db.TenantMismatchMode = TenantMismatchMode.Throw;
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                db.Entry(blog1).Property("TenantId").CurrentValue = "77";
+                await Assert.ThrowsAsync<MultiTenantException>(() => db.SaveChangesAsync());
+            }
+
+            // TenantMismatchMode.Ignore
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                db.TenantMismatchMode = TenantMismatchMode.Ignore;
+
+                var blog1 = new Blog { Title = "34" };
+                db.Blogs?.Add(blog1);
+                db.Entry(blog1).Property("TenantId").CurrentValue = "34";
+                await db.SaveChangesAsync();
+                Assert.Equal("34", db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+
+            // TenantMismatchMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+                db.TenantMismatchMode = TenantMismatchMode.Overwrite;
+
+                var blog1 = new Blog { Title = "77" };
+                db.Blogs?.Add(blog1);
+                db.Entry(blog1).Property("TenantId").CurrentValue = "77";
+                await db.SaveChangesAsync();
+                Assert.Equal(tenant1.Id, db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantNotSetWhenUpdatingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantNotSetMode.Throw
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantNotSetMode = TenantNotSetMode.Throw;
+                db.Entry(blog1).Property("TenantId").CurrentValue = null;
+                await Assert.ThrowsAsync<MultiTenantException>(() => db.SaveChangesAsync());
+            }
+
+            // TenantNotSetMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc12" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantNotSetMode = TenantNotSetMode.Overwrite;
+                db.Entry(blog1).Property("TenantId").CurrentValue = null;
+                await db.SaveChangesAsync();
+
+                Assert.Equal(tenant1.Id, db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantMismatchWhenUpdatingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantMismatchMode.Throw
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Throw;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "11";
+                await Assert.ThrowsAsync<MultiTenantException>(() => db.SaveChangesAsync());
+            }
+
+            // TenantMismatchMode.Ignore
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Ignore;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "11";
+                await db.SaveChangesAsync();
+                Assert.Equal("11", db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+
+            // TenantMismatchMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc12" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Overwrite;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "11";
+                await db.SaveChangesAsync();
+                Assert.Equal(tenant1.Id, db.Entry(blog1).Property("TenantId").CurrentValue);
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantNotSetWhenDeletingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantNotSetMode.Throw
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantNotSetMode = TenantNotSetMode.Throw;
+                db.Entry(blog1).Property("TenantId").CurrentValue = null;
+                db.Blogs?.Remove(blog1);
+                await Assert.ThrowsAsync<MultiTenantException>(() => db.SaveChangesAsync());
+            }
+
+            // TenantNotSetMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantNotSetMode = TenantNotSetMode.Overwrite;
+                db.Entry(blog1).Property("TenantId").CurrentValue = null;
+                db.Blogs?.Remove(blog1);
+                Assert.Equal(1, await db.SaveChangesAsync());
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
+
+    [Fact]
+    public async Task HandleTenantMismatchWhenDeletingAsync()
+    {
+        try
+        {
+            await _connection.OpenAsync();
+            var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
+
+            // TenantMismatchMode.Throw
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Throw;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "17";
+                db.Blogs?.Remove(blog1);
+                await Assert.ThrowsAsync<MultiTenantException>(() => db.SaveChangesAsync());
+            }
+
+            // TenantMismatchMode.Ignore
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Ignore;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "17";
+                db.Blogs?.Remove(blog1);
+                Assert.Equal(1, await db.SaveChangesAsync());
+            }
+
+            // TenantMismatchMode.Overwrite
+            await using (var db = new TestDbContext(tenant1, _options))
+            {
+                await db.Database.EnsureDeletedAsync();
+                await db.Database.EnsureCreatedAsync();
+
+                var blog1 = new Blog { Title = "abc" };
+                db.Blogs?.Add(blog1);
+                await db.SaveChangesAsync();
+
+                db.TenantMismatchMode = TenantMismatchMode.Overwrite;
+                db.Entry(blog1).Property("TenantId").CurrentValue = "17";
+                db.Blogs?.Remove(blog1);
+                Assert.Equal(1, await db.SaveChangesAsync());
+            }
+        }
+        finally
+        {
+            await _connection.CloseAsync();
+        }
+    }
 }
--- test/Finbuckle.MultiTenant.EntityFrameworkCore.Test/MultiTenantDbContext/MultiTenantDbContextShould.cs
+++ test/Finbuckle.MultiTenant.EntityFrameworkCore.Test/MultiTenantDbContext/MultiTenantDbContextShould.cs
@@ -3,6 +3,7 @@
 
 using Finbuckle.MultiTenant.Abstractions;
 using Finbuckle.MultiTenant.Extensions;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -26,27 +27,21 @@ public class MultiTenantDbContextShould
     [Fact]
     public void WorkWithSingleParamCtor()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
-        var mca = new StaticMultiTenantContextAccessor<TenantInfo>(tenant1);
-        var c = new TestBlogDbContext(mca);
-
+        var c = new TestBlogDbContext();
         Assert.NotNull(c);
     }
 
     [Fact]
     public void WorkWithTwoParamCtor()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
-        var mca = new StaticMultiTenantContextAccessor<TenantInfo>(tenant1);
-        var c = new TestBlogDbContext(mca, new DbContextOptions<TestBlogDbContext>());
-
+        var c = new TestBlogDbContext(new DbContextOptions<TestBlogDbContext>());
         Assert.NotNull(c);
     }
 
     [Fact]
     public void WorkWithCreateDbOptions()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
         var c =
             EntityFrameworkCore.MultiTenantDbContext.Create<TestBlogDbContext, TenantInfo>(tenant1,
                 new DbContextOptions<TestBlogDbContext>());
@@ -57,7 +52,7 @@ public class MultiTenantDbContextShould
     [Fact]
     public void WorkWithCreateDependencies()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
         var c =
             EntityFrameworkCore.MultiTenantDbContext.Create<TestBlogDbContext, TenantInfo>(tenant1, new object());
 
@@ -69,20 +64,21 @@ public class MultiTenantDbContextShould
     {
         // create a sp
         var services = new ServiceCollection();
-        services.AddTransient<object>(sp => 42);
+        services.AddTransient<object>(_ => 42);
         var sp = services.BuildServiceProvider();
 
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
         var c =
             EntityFrameworkCore.MultiTenantDbContext.Create<TestBlogDbContext, TenantInfo>(tenant1, sp);
 
         Assert.NotNull(c);
+        Assert.Same(tenant1, c.TenantInfo);
     }
 
     [Fact]
     public void WorkWithCreateNoOptions()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
         var c = EntityFrameworkCore.MultiTenantDbContext.Create<TestBlogDbContext, TenantInfo>(tenant1);
 
         Assert.NotNull(c);
@@ -91,9 +87,35 @@ public class MultiTenantDbContextShould
     [Fact]
     public void ThrowOnInvalidDbContext()
     {
-        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc", Name = "abc" };
+        // Passing args that no constructor accepts should throw ArgumentException.
+        var tenant1 = new TenantInfo { Id = "abc", Identifier = "abc" };
 
         Assert.Throws<ArgumentException>(() =>
-            EntityFrameworkCore.MultiTenantDbContext.Create<DbContext, TenantInfo>(tenant1));
+            EntityFrameworkCore.MultiTenantDbContext.Create<TestBlogDbContext, TenantInfo>(tenant1, new object(), new object()));
+    }
+
+    [Fact]
+    public void QueryFilterIsolatesDataByTenant()
+    {
+        using var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+        var options = new DbContextOptionsBuilder().UseSqlite(connection).Options;
+
+        var tenant1 = new TenantInfo { Id = "t1", Identifier = "t1" };
+        var tenant2 = new TenantInfo { Id = "t2", Identifier = "t2" };
+
+        using var setup = new TestBlogDbContext(options);
+        setup.TenantInfo = tenant1;
+        setup.Database.EnsureCreated();
+        setup.Blogs?.Add(new Blog { Title = "tenant1 blog" });
+        setup.SaveChanges();
+
+        using var asT2 = new TestBlogDbContext(options);
+        asT2.TenantInfo = tenant2;
+        Assert.Equal(0, asT2.Blogs?.Count());
+
+        using var asT1 = new TestBlogDbContext(options);
+        asT1.TenantInfo = tenant1;
+        Assert.Equal(1, asT1.Blogs?.Count());
     }
 }
