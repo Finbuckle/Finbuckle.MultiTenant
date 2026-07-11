58ee2a2f48057f41b47724b846efb7979931b1d6
Andrew White <andrew@finbuckle.com>
2026-07-11 12:57:17 -0600

    fix(finbuckle): improve service decoration for keyed registrations and validation

--- src/Finbuckle.MultiTenant/Extensions/ServiceCollectionExtensions.cs
+++ src/Finbuckle.MultiTenant/Extensions/ServiceCollectionExtensions.cs
@@ -56,7 +56,7 @@ public static class ServiceCollectionExtensions
 
 
     /// <summary>
-    /// Decorates an existing service registration with a new implementation that wraps the original.
+    /// Decorates existing unkeyed service registrations with a new implementation that wraps the original.
     /// </summary>
     /// <typeparam name="TService">The service type to decorate.</typeparam>
     /// <typeparam name="TImpl">The decorator implementation type.</typeparam>
@@ -64,83 +64,76 @@ public static class ServiceCollectionExtensions
     /// <param name="parameters">Additional parameters to pass to the decorator constructor after the inner service.</param>
     /// <returns><c>true</c> if the service was successfully decorated.</returns>
     /// <exception cref="ArgumentException">Thrown when no service of type <typeparamref name="TService"/> is found.</exception>
-    /// <exception cref="Exception">Thrown when the service cannot be instantiated.</exception>
+    /// <remarks>
+    /// Keyed registrations are not changed. Calling this method repeatedly stacks decorators around the existing
+    /// registration. A decorator that wraps a disposable inner service should forward disposal as appropriate.
+    /// </remarks>
     public static bool DecorateService<TService, TImpl>(this IServiceCollection services, params object[] parameters)
     {
-        var existingServices = services.Where(s => s.ServiceType == typeof(TService)).ToList();
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(parameters);
+
+        if (!typeof(TService).IsAssignableFrom(typeof(TImpl)))
+            throw new ArgumentException(
+                $"Decorator type {typeof(TImpl).Name} must implement or inherit from {typeof(TService).Name}.",
+                nameof(TImpl));
+
+        var parametersCopy = parameters.ToArray();
+        var existingServices = services.Select((descriptor, index) => (descriptor, index))
+            .Where(item => item.descriptor.ServiceType == typeof(TService) && !item.descriptor.IsKeyedService)
+            .ToList();
         if (existingServices.Count == 0)
             throw new ArgumentException($"No service of type {typeof(TService).Name} found.");
 
-        foreach (var existingService in existingServices)
-        {
-            ServiceDescriptor? newService;
-            if (existingService.ImplementationType is not null)
-            {
-                newService = new ServiceDescriptor(existingService.ServiceType,
-                    sp =>
-                    {
-                        TService inner =
-                            (TService)ActivatorUtilities.CreateInstance(sp, existingService.ImplementationType);
+        var replacements = existingServices.Select(item =>
+            (item.index, descriptor: CreateDecoratedDescriptor<TService, TImpl>(item.descriptor, parametersCopy)))
+            .ToList();
 
-                        if (inner is null)
-                            throw new ArgumentException(
-                                $"Unable to instantiate decorated type via implementation type {existingService.ImplementationType.Name}.");
-
-                        var parameters2 = new object[parameters.Length + 1];
-                        Array.Copy(parameters, 0, parameters2, 1, parameters.Length);
-                        parameters2[0] = inner;
-
-                        return ActivatorUtilities.CreateInstance<TImpl>(sp, parameters2)!;
-                    },
-                    existingService.Lifetime);
-            }
-            else if (existingService.ImplementationInstance is not null)
-            {
-                newService = new ServiceDescriptor(existingService.ServiceType,
-                    sp =>
-                    {
-                        TService inner = (TService)existingService.ImplementationInstance;
-                        if (inner is null)
-                            throw new ArgumentException(
-                                $"Unable to instantiate decorated type via implementation instance of type {existingService.ImplementationInstance.GetType().Name}.");
-
-                        var parameters2 = new object[parameters.Length + 1];
-                        Array.Copy(parameters, 0, parameters2, 1, parameters.Length);
-                        parameters2[0] = inner;
-
-                        return ActivatorUtilities.CreateInstance<TImpl>(sp, parameters2)!;
-                    },
-                    existingService.Lifetime);
-            }
-            else if (existingService.ImplementationFactory is not null)
-            {
-                newService = new ServiceDescriptor(existingService.ServiceType,
-                    sp =>
-                    {
-                        TService inner = (TService)existingService.ImplementationFactory(sp);
-                        if (inner is null)
-                            throw new ArgumentException(
-                                $"Unable to instantiate decorated type via implementation factory for type {existingService.ServiceType}.");
-
-                        var parameters2 = new object[parameters.Length + 1];
-                        Array.Copy(parameters, 0, parameters2, 1, parameters.Length);
-                        parameters2[0] = inner;
-
-                        return ActivatorUtilities.CreateInstance<TImpl>(sp, parameters2)!;
-                    },
-                    existingService.Lifetime);
-            }
-            else
-            {
-                throw new ArgumentException(
-                    "Unable to instantiate decorated type.");
-            }
-
-            services.Remove(existingService);
-            services.Add(newService);
-        }
+        foreach (var replacement in replacements)
+            services[replacement.index] = replacement.descriptor;
 
         return true;
+    }
+
+    private static ServiceDescriptor CreateDecoratedDescriptor<TService, TImpl>(
+        ServiceDescriptor existingService, object[] parameters)
+    {
+        Func<IServiceProvider, object?> createInner;
+
+        if (existingService.ImplementationType is not null)
+        {
+            var implementationType = existingService.ImplementationType;
+            createInner = sp => ActivatorUtilities.CreateInstance(sp, implementationType);
+        }
+        else if (existingService.ImplementationInstance is not null)
+        {
+            var implementationInstance = existingService.ImplementationInstance;
+            createInner = _ => implementationInstance;
+        }
+        else if (existingService.ImplementationFactory is not null)
+        {
+            createInner = existingService.ImplementationFactory;
+        }
+        else
+        {
+            throw new ArgumentException("Unable to instantiate decorated type.");
+        }
+
+        return new ServiceDescriptor(existingService.ServiceType,
+            sp =>
+            {
+                var innerObject = createInner(sp);
+                if (innerObject is not TService inner)
+                    throw new ArgumentException(
+                        $"Unable to instantiate decorated service of type {typeof(TService).Name}.");
+
+                var constructorParameters = new object[parameters.Length + 1];
+                constructorParameters[0] = inner;
+                Array.Copy(parameters, 0, constructorParameters, 1, parameters.Length);
+
+                return ActivatorUtilities.CreateInstance<TImpl>(sp, constructorParameters)!;
+            },
+            existingService.Lifetime);
     }
 
     /// <summary>
